f59ee6c596da9e3e84d4aa0540e90ad7170bdc7d
Yevhen Malyshev <meugen.82@gmail.com>
2025-10-31 20:28:14 +0200

    Implemented validations && refactoring

--- src/main/java/meugeninua/masterdetails/controllers/MasterController.java
+++ src/main/java/meugeninua/masterdetails/controllers/MasterController.java
@@ -1,9 +1,8 @@
 package meugeninua.masterdetails.controllers;
 
+import jakarta.validation.Valid;
 import meugeninua.masterdetails.dto.MasterDto;
-import meugeninua.masterdetails.prrocessors.Processor;
 import meugeninua.masterdetails.services.MasterService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -15,36 +14,32 @@ import java.util.stream.Stream;
 public class MasterController {
 
     private final MasterService masterService;
-    private final Processor masterProcessor;
 
-    public MasterController(
-        MasterService masterService,
-        @Qualifier("master") Processor masterProcessor
-    ) {
+    public MasterController(MasterService masterService) {
         this.masterService = masterService;
-        this.masterProcessor = masterProcessor;
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Stream<?> findAll() {
-        return masterService.findAll().map(masterProcessor::process);
+        return masterService.findAll();
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Object findById(@PathVariable("id") Long id) {
-        var result = masterService.findById(id);
-        return masterProcessor.process(result);
+        return masterService.findById(id);
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public Object create(@RequestBody MasterDto master) {
-        var result = masterService.create(master);
-        return masterProcessor.process(result);
+    public Object create(@RequestBody @Valid MasterDto master) {
+        return masterService.create(master);
     }
 
     @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public MasterDto update(@PathVariable("id") Long id, @RequestBody MasterDto master) {
+    public Object update(
+        @PathVariable("id") Long id,
+        @RequestBody @Valid MasterDto master
+    ) {
         return masterService.update(id, master);
     }
 
--- src/main/java/meugeninua/masterdetails/services/DetailService.java
+++ src/main/java/meugeninua/masterdetails/services/DetailService.java
@@ -2,8 +2,10 @@ package meugeninua.masterdetails.services;
 
 import meugeninua.masterdetails.dto.DetailDto;
 import meugeninua.masterdetails.mappers.DetailEntityMapper;
+import meugeninua.masterdetails.prrocessors.Processor;
 import meugeninua.masterdetails.repositories.DetailRepository;
 import meugeninua.masterdetails.repositories.MasterRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,21 +17,24 @@ import java.util.stream.StreamSupport;
 @Service
 public class DetailService {
 
-    private MasterRepository masterRepository;
+    private final MasterRepository masterRepository;
     private final DetailRepository detailRepository;
     private final DetailEntityMapper detailMapper;
+    private final Processor detailProcessor;
 
     public DetailService(
         MasterRepository masterRepository,
         DetailRepository detailRepository,
-        DetailEntityMapper detailMapper
+        DetailEntityMapper detailMapper,
+        @Qualifier("detail") Processor detailProcessor
     ) {
         this.masterRepository = masterRepository;
         this.detailRepository = detailRepository;
         this.detailMapper = detailMapper;
+        this.detailProcessor = detailProcessor;
     }
 
-    public Stream<DetailDto> findAll(Long masterId) {
+    public Stream<?> findAll(Long masterId) {
         if (!masterRepository.existsById(masterId)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
@@ -37,19 +42,20 @@ public class DetailService {
             detailRepository.findAllByMasterIdEquals(masterId).spliterator(),
             false
         );
-        return stream.map(detailMapper::mapToDto);
+        return stream.map(detailMapper::mapToDto).map(detailProcessor::process);
     }
 
-    public DetailDto findById(Long masterId, Long detailId) {
+    public Object findById(Long masterId, Long detailId) {
         return detailRepository.findByMasterIdEqualsAndIdEquals(masterId, detailId)
             .map(detailMapper::mapToDto)
+            .map(detailProcessor::process)
             .orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
             );
     }
 
     @Transactional
-    public DetailDto create(Long masterId, DetailDto detailDto) {
+    public Object create(Long masterId, DetailDto detailDto) {
         var master = masterRepository.findById(masterId).orElseThrow(
             () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
         );
@@ -57,11 +63,12 @@ public class DetailService {
         entity.setId(null);
         entity = detailRepository.save(entity);
 
-        return detailMapper.mapToDto(entity);
+        var result = detailMapper.mapToDto(entity);
+        return detailProcessor.process(result);
     }
 
     @Transactional
-    public DetailDto update(Long masterId, Long detailId, DetailDto detailDto) {
+    public Object update(Long masterId, Long detailId, DetailDto detailDto) {
         if (!detailRepository.existsByMasterIdEqualsAndIdEquals(masterId, detailId)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
@@ -72,7 +79,8 @@ public class DetailService {
         entity.setId(detailId);
         entity = detailRepository.save(entity);
 
-        return detailMapper.mapToDto(entity);
+        var result = detailMapper.mapToDto(entity);
+        return detailProcessor.process(result);
     }
 
     @Transactional
