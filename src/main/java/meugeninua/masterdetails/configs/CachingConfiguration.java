06154807398023ed50256dd73caef2909ad8828c
Yevhen Malyshev <meugen.82@gmail.com>
2025-11-25 13:11:52 +0200

    Added use SSL option for redis

--- src/main/java/meugeninua/masterdetails/configs/CachingConfiguration.java
+++ src/main/java/meugeninua/masterdetails/configs/CachingConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 @Configuration
@@ -20,11 +21,16 @@ public class CachingConfiguration {
     public RedisConnectionFactory jedisConnectionFactory(Environment environment) {
         var hostname = environment.getProperty("REDIS_HOSTNAME", "localhost");
         var port = environment.getProperty("REDIS_PORT", Integer.class, 6379);
+        var useSsl = environment.getProperty("REDIS_USE_SSL", Boolean.class, false);
 
         logger.info("Redis hostname: {}, port: {}", hostname, port);
 
-        return new JedisConnectionFactory(
-            new RedisStandaloneConfiguration(hostname, port)
-        );
+        var configuration = new RedisStandaloneConfiguration(hostname, port);
+        var builder = JedisClientConfiguration.builder();
+        if (useSsl) {
+            builder.useSsl();
+        }
+
+        return new JedisConnectionFactory(configuration, builder.build());
     }
 }
