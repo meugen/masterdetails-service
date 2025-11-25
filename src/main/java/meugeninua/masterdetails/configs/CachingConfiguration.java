b43077b27893d4984a7c50003b6495f6c6a785be
Yevhen Malyshev <meugen.82@gmail.com>
2025-11-25 13:36:26 +0200

    Removed logs

--- src/main/java/meugeninua/masterdetails/configs/CachingConfiguration.java
+++ src/main/java/meugeninua/masterdetails/configs/CachingConfiguration.java
@@ -1,7 +1,5 @@
 package meugeninua.masterdetails.configs;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,15 +13,11 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 @EnableCaching
 public class CachingConfiguration {
 
-    private final Logger logger = LoggerFactory.getLogger(CachingConfiguration.class);
-
     @Bean
     public RedisConnectionFactory jedisConnectionFactory(Environment environment) {
         var hostname = environment.getProperty("REDIS_HOSTNAME", "localhost");
         var port = environment.getProperty("REDIS_PORT", Integer.class, 6379);
         var useSsl = environment.getProperty("REDIS_USE_SSL", Boolean.class, false);
-
-        logger.info("Redis hostname: {}, port: {}", hostname, port);
 
         var configuration = new RedisStandaloneConfiguration(hostname, port);
         var clientConfigBuilder = JedisClientConfiguration.builder();
