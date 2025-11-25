5c6ccccda3a01a6a7cee1d0df1e6dd5061b4bfad
Yevhen Malyshev <meugen.82@gmail.com>
2025-11-25 12:39:38 +0200

    Loged Redis hostname and port

--- src/main/java/meugeninua/masterdetails/configs/CachingConfiguration.java
+++ src/main/java/meugeninua/masterdetails/configs/CachingConfiguration.java
@@ -1,5 +1,7 @@
 package meugeninua.masterdetails.configs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +14,15 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 @EnableCaching
 public class CachingConfiguration {
 
+    private final Logger logger = LoggerFactory.getLogger(CachingConfiguration.class);
+
     @Bean
     public RedisConnectionFactory jedisConnectionFactory(Environment environment) {
         var hostname = environment.getProperty("REDIS_HOSTNAME", "localhost");
         var port = environment.getProperty("REDIS_PORT", Integer.class, 6379);
+
+        logger.info("Redis hostname: {}, port: {}", hostname, port);
+
         return new JedisConnectionFactory(
             new RedisStandaloneConfiguration(hostname, port)
         );
