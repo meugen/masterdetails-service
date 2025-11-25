1a2f983194c3e4a13ed37b0ceaf5117e1e4c9f02
Yevhen Malyshev <meugen.82@gmail.com>
2025-11-25 19:45:43 +0200

    Fixed Redis logging format

--- src/main/java/meugeninua/masterdetails/configs/CachingConfiguration.java
+++ src/main/java/meugeninua/masterdetails/configs/CachingConfiguration.java
@@ -1,6 +1,5 @@
 package meugeninua.masterdetails.configs;
 
-import org.jboss.logging.Logger;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +9,13 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
+import java.util.logging.Logger;
+
 @Configuration
 @EnableCaching
 public class CachingConfiguration {
 
-    private final Logger logger = Logger.getLogger(CachingConfiguration.class);
+    private final Logger logger = Logger.getLogger(CachingConfiguration.class.getName());
 
     @Bean
     public RedisConnectionFactory jedisConnectionFactory(Environment environment) {
@@ -22,7 +23,7 @@ public class CachingConfiguration {
         var port = environment.getProperty("REDIS_PORT", Integer.class, 6379);
         var useSsl = environment.getProperty("REDIS_USE_SSL", Boolean.class, false);
 
-        logger.infov("Redis hostname: {}, port: {}, use-ssl: {}", hostname, port, useSsl);
+        logger.info(String.format("Redis hostname: %s, port: %d, use-ssl: %b", hostname, port, useSsl));
 
         var configuration = new RedisStandaloneConfiguration(hostname, port);
         var clientConfigBuilder = JedisClientConfiguration.builder();
