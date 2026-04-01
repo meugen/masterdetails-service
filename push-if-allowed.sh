41300bd7d21c39afa3aec86831eec7330e4ec44f
Yevgeniy Malyshev <meugen.82@gmail.com>
2026-04-02 03:34:36 +1100

    Feature/push if allowed script (#24)
    * Implemented push-if-allowed.sh script
    
    * Fixed bugs
    
    * Disabled cache for maven build
    
    * Added clean before test
    
    * Implemented custom .m2 repository

---
+++ push-if-allowed.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+docker run -d -p 5433:5432 --name test_postgres \
+  -e POSTGRES_PASSWORD=test_password \
+  -e POSTGRES_USER=masterdetails \
+  -e POSTGRES_DB=masterdetails \
+   postgres:17-alpine || exit 1
+docker run -d -p 6378:6379 --name test_redis redis:8.2-alpine || exit 1
+
+export PGSQL_HOSTNAME=localhost
+export PGSQL_PORT=5433
+export PGSQL_USERNAME=masterdetails
+export PGSQL_PASSWORD=test_password
+export REDIS_HOSTNAME=localhost
+export REDIS_PORT=6378
+
+./mvnw clean test \
+  -Dmaven.repo.local=./.m2/$(uuidgen) \
+  -Dspring.jpa.hibernate.ddl-auto=create
+RESULT=$?
+
+docker stop test_postgres test_redis
+docker rm test_postgres test_redis
+
+if [ $RESULT -eq 0 ]; then
+  git push
+fi
