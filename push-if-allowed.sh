2a2810b05aaa0fe724f533e78894f4645182ded2
Yevhen Malyshev <meugen.82@gmail.com>
2026-04-01 15:59:14 +0000

    Fixed bugs

--- push-if-allowed.sh
+++ push-if-allowed.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker run -d -p 5433:5432 --name test_postgres /
-  -e POSTGRES_PASSWORD=test_password /
-  -e POSTGRES_USER=masterdetails /
-  -e POSTGRES_DB=masterdetails /
+docker run -d -p 5433:5432 --name test_postgres \
+  -e POSTGRES_PASSWORD=test_password \
+  -e POSTGRES_USER=masterdetails \
+  -e POSTGRES_DB=masterdetails \
    postgres:17-alpine || exit 1
 docker run -d -p 6378:6379 --name test_redis redis:8.2-alpine || exit 1
 
